8c7279f07e379c5edc7423334525ad6813093c55
VarunPareek <114807315+varunpareek690@users.noreply.github.com>
2024-06-11 18:21:23 +0530

    first commit

---
+++ backend/Brudial/src/main/java/com/BruDial/Brudial/BrudialApplication.java
@@ -0,0 +1,13 @@
+package com.BruDial.Brudial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrudialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrudialApplication.class, args);
+	}
+
+}
---
+++ backend/Brudial/src/test/java/com/BruDial/Brudial/BrudialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BruDial.Brudial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrudialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
